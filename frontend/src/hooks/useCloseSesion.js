24a6323c44242e3750ad408a492dc58a79eb74b6
smith367 <brayansmithpuente@gmail.com>
2023-11-10 23:09:14 -0500

    close sesion fix

--- frontend/src/hooks/useCloseSesion.js
+++ frontend/src/hooks/useCloseSesion.js
@@ -6,7 +6,7 @@ const useCloseSesion = (setUserDataStatus) => {
     });
     localStorage.removeItem("login");
 
-    localStorage.removeItem("name");
+    localStorage.removeItem("user");
 
     localStorage.removeItem("email");
 };
